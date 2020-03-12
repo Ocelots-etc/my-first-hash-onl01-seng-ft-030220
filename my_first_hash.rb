f6d5933c5402923c953f5ffb2f15a6b626f173bd
Cat Gomez <catherinedgomez@gmail.com>
2020-03-12 21:57:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -17,9 +17,7 @@ def retrieval
                   "oil paintings" => 3
                  }
   shipping_manifest["oil paintings"]
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
-end
+  end
 
 def adding
   shipping_manifest = {
